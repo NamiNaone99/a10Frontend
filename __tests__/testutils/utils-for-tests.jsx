7e0e51aa34841ab49b4e36e575d7c7b9229d4398
NamiNaone99 <nongft09090@gmail.com>
2025-03-24 00:42:03 +0700

    trig test

--- __tests__/testutils/utils-for-tests.jsx
+++ __tests__/testutils/utils-for-tests.jsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react'
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import { bookSlice } from '@/redux/features/bookSlice'
-
+// 
 export function renderWithProviders(
     ui,
     {
