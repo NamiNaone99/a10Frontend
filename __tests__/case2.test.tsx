3e80ddd677750728eb06ac20c129f2ff9d63dccd
NamiNaone99 <nongft09090@gmail.com>
2025-03-24 00:44:36 +0700

    add lost file

--- __tests__/case2.test.tsx
+++ __tests__/case2.test.tsx
@@ -2,7 +2,7 @@ import '@testing-library/jest-dom'
 import { screen, render, waitFor } from '@testing-library/react'
 import BookingList from '@/components/BookingList';
 import { renderWithProviders } from './testutils/utils-for-tests'
-// 
+
 describe('Booking Page', () => {
   it('Booking page get functionality', () => {
     renderWithProviders(<BookingList/>)
